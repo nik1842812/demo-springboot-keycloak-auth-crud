ed573f8b6b08209ae38d77d33e0fe4461643d75e
Mbacke Mbaye <mbackembaye74@gmail.com>
2025-09-11 16:33:14 +0000

    add SecurityConfig class to configure Spring Security with OAuth2 support and role-based access control

---
+++ src/main/java/sn/malcolm/demo/security/SecurityConfig.java
@@ -0,0 +1,73 @@
+package sn.malcolm.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import sn.malcolm.demo.model.enums.UserRole;
+import sn.malcolm.demo.security.oauth2.OAuth2AuthenticationFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private OAuth2AuthenticationFilter oAuth2AuthenticationFilter;
+
+    @Bean
+    public org.springframework.security.web.SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/proxy/keycloak/**").permitAll()
+                        .requestMatchers("/api/reset-password", "/api/reset-password/verify-token", "/api/reset-password/reset", "api/my-users/change-password").permitAll()
+                        .requestMatchers("/token/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/callback").permitAll()
+                        .requestMatchers("/api/users/create").permitAll()
+                        .requestMatchers("/api/auth/sign-in").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/download/**").permitAll()
+                        .requestMatchers("/api/users/**").hasAnyRole(UserRole.ADMIN.getValue())
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .addFilterBefore(oAuth2AuthenticationFilter, BasicAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+            if (realmAccess != null && realmAccess.containsKey("roles")) {
+                @SuppressWarnings("unchecked")
+                List<String> roles = (List<String>) realmAccess.get("roles");
+
+                return roles.stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .collect(Collectors.toList());
+            }
+            return new ArrayList<>();
+        });
+
+        return converter;
+    }
+}
